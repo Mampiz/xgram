41fcad7c38cd9f223487de5853aaa7b56e80d7af
Mampiz <josepmampel20@gmail.com>
2024-07-15 21:51:56 +0200

    Sidebar fix

--- frontend/xgram/src/components/users.tsx
+++ frontend/xgram/src/components/users.tsx
@@ -34,7 +34,7 @@ const ParentComponent = ({user}: ParentComponentProps) => {
 	return (
 		<div className="flex h-screen w-screen">
 			<aside className="w-1/6 bg-white border-r">
-				<Sidebar />
+				<Sidebar user={user}/>
 			</aside>
 			<main className="flex-1 flex items-center justify-center bg-gray-100">
 				<div className="App">{posts.length > 0 && <Post post={posts[currentIndex]} onNextPost={handleNextPost} userid={user ? user.id : null} />}</div>
